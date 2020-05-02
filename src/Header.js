b77b554eb95aa3b67666f1ce02f2abb0238280f5
Johnson Le Thieu <johnsonle2929@gmail.com>
2020-05-02 03:45:26 -0700

    added img

--- src/Header.js
+++ src/Header.js
@@ -5,11 +5,11 @@ class Header extends Component {
   render() {
     return (
       <header className="resume-header container">
-        <img className="headshot" alt="myheadshots" src="/../../public/headshot.png"/>
+        <img className="headshot" alt="myheadshot" src="headshot.png"/>
         <h1 className="resume-title">Johnson Le Thieu</h1>
         <h2><i>Full-Stack Software Engineer</i></h2>
         <p>(626) 346-4095 | johnsonle2929@gmail.com</p>
-        <p><i><a href="https://www.linkedin.com/in/johnson-thieu/">linkedin.com/in/johnson-thieu</a> | <a href="">github.com/MyNameIsJohnson</a></i></p>
+        <p><i><a href="https://www.linkedin.com/in/johnson-thieu/">linkedin.com/in/johnson-thieu</a> | <a href="https://github.com/MyNameIsJohnson">github.com/MyNameIsJohnson</a></i></p>
         <p>San Francisco Bay Area, California</p>
       </header>
     );
