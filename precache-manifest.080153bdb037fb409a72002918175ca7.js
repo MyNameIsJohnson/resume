308b0a3dcd6b03d37cff6a0ab11fe08c1003c63a
Johnson Le Thieu <johnsonle2929@gmail.com>
2020-05-02 02:45:21 -0700

    Updates

---
+++ precache-manifest.080153bdb037fb409a72002918175ca7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83032481249e12fa64044f6e162c48ed",
+    "url": "/gh-pages-resume/index.html"
+  },
+  {
+    "revision": "439901fc46e07f9ff461",
+    "url": "/gh-pages-resume/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "53b16adb80ae093433cb",
+    "url": "/gh-pages-resume/static/css/main.1bc6d0ad.chunk.css"
+  },
+  {
+    "revision": "439901fc46e07f9ff461",
+    "url": "/gh-pages-resume/static/js/2.11011eb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gh-pages-resume/static/js/2.11011eb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53b16adb80ae093433cb",
+    "url": "/gh-pages-resume/static/js/main.f29eff6d.chunk.js"
+  },
+  {
+    "revision": "c9ebacf6d794093bc1b4",
+    "url": "/gh-pages-resume/static/js/runtime-main.d0f7c7bb.js"
+  }
+]);
