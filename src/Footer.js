0a6c8250c570b4e55a7e847a5e2c437528746666
Johnson Le Thieu <johnsonle2929@gmail.com>
2020-05-04 15:27:31 -0700

    added footer

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Footer extends Component {
+   
+  render() {
+    return (
+      <footer className="resume-footer container">
+        <p>(626) 346-4095 | johnsonle2929@gmail.com</p>
+        <p><i><a href="https://www.linkedin.com/in/johnson-thieu/">linkedin.com/in/johnson-thieu</a> | <a href="https://github.com/MyNameIsJohnson">github.com/MyNameIsJohnson</a></i> San Francisco Bay Area, California</p>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
