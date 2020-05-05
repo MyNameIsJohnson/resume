8859bc5b30e066bb1883452ddb28d8673ad34d48
Johnson Le Thieu <johnsonle2929@gmail.com>
2020-05-04 19:30:22 -0700

    footer img

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,7 @@ class Footer extends Component {
   render() {
     return (
       <footer className="resume-footer container">
-        <p>This page was built using React <img src="favicon.ico"/></p>
+        <p>This page was built using React <img class=".footer-img" src="favicon.ico"/></p>
         <p>(626) 346-4095 | johnsonle2929@gmail.com</p>
         <p><i><a href="https://www.linkedin.com/in/johnson-thieu/">linkedin.com/in/johnson-thieu</a> | <a href="https://github.com/MyNameIsJohnson">github.com/MyNameIsJohnson</a></i> San Francisco Bay Area, California</p>
       </footer>
